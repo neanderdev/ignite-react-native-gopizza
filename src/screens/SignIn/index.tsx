b1c1db63963f23c9d609db5404ebefa7622a18db
neanderdev <neander.faria@hotmail.com>
2022-08-23 09:47:49 -0300

    Finalizando do SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,30 +1,54 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
-import { Container } from './styles';
+import {
+    Container,
+    Content,
+    Title,
+    Brand,
+    ForgotPasswordButton,
+    ForgotPasswordLabel
+} from './styles';
+
+import brandImg from '@assets/brand.png';
 
 export function SignIn() {
     return (
         <Container>
-            <Input
-                placeholder='E-mail'
-                type='secondary'
-                autoCorrect={false}
-                autoCapitalize='none'
-            />
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+                <Content>
+                    <Brand source={brandImg} />
 
-            <Input
-                placeholder='Senha'
-                type='secondary'
-                secureTextEntry
-            />
+                    <Title>Login</Title>
 
-            <Button
-                title='Entrar'
-                type='secondary'
-            />
+                    <Input
+                        placeholder='E-mail'
+                        type='secondary'
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                    />
+
+                    <Input
+                        placeholder='Senha'
+                        type='secondary'
+                        secureTextEntry
+                    />
+
+                    <ForgotPasswordButton>
+                        <ForgotPasswordLabel>
+                            Esqueci minha senha
+                        </ForgotPasswordLabel>
+                    </ForgotPasswordButton>
+
+                    <Button
+                        title='Entrar'
+                        type='secondary'
+                    />
+                </Content>
+            </KeyboardAvoidingView>
         </Container>
     );
 }
