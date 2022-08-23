8761f3c4509d3dc60744ac00618410db3bb93aaf
neanderdev <neander.faria@hotmail.com>
2022-08-23 14:47:08 -0300

    Componente de Input de Preço

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -4,6 +4,7 @@ import * as ImagePicker from 'expo-image-picker';
 
 import { ButtonBack } from '@components/ButtonBack';
 import { Photo } from '@components/Photo';
+import { InputPrice } from '@components/InputPrice';
 
 import {
     Container,
@@ -63,6 +64,10 @@ export function Product() {
                     isLoading={isLoading}
                 />
             </Upload>
+
+            <InputPrice size='P' />
+            <InputPrice size='M' />
+            <InputPrice size='G' />
         </Container>
     );
 }
