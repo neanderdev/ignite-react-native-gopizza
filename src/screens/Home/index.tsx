cccfbffb7a8d5ce34467aa3b03d61bbe21c919df
neanderdev <neander.faria@hotmail.com>
2022-08-23 17:01:47 -0300

    Buscando pizzas cadastradas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 import { useTheme } from 'styled-components/native';
 import { MaterialIcons } from '@expo/vector-icons';
+import firestore from '@react-native-firebase/firestore';
 
 import { Search } from '@components/Search';
-import { ProductCard } from '@components/ProductCard';
+import { ProductCard, ProductProps } from '@components/ProductCard';
 
 import {
     Container,
@@ -21,6 +23,32 @@ import happyEmoji from '@assets/happy.png';
 
 export function Home() {
     const { COLORS } = useTheme();
+
+    async function fetchPizzas(value: string) {
+        const formattedValue = value.toLocaleLowerCase().trim();
+
+        firestore()
+            .collection('pizzas')
+            .orderBy('name_insensitive')
+            .startAt(formattedValue)
+            .endAt(`${formattedValue}\uf8ff`)
+            .get()
+            .then((response) => {
+                const data = response.docs.map((doc) => {
+                    return {
+                        id: doc.id,
+                        ...doc.data(),
+                    };
+                }) as ProductProps[];
+
+                console.log(data);
+            })
+            .catch(() => Alert.alert('Consulta', 'Não foi possível realizar a consulta.'));
+    }
+
+    useEffect(() => {
+        fetchPizzas('');
+    }, []);
 
     return (
         <Container>
