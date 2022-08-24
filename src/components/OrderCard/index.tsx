579c6406b8c29853287e944c99ebdb7636b3a143
neanderdev <neander.faria@hotmail.com>
2022-08-24 14:18:07 -0300

    Componente de Pedido

--- src/components/OrderCard/index.tsx
+++ src/components/OrderCard/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { TouchableOpacityProps } from 'react-native';
 
-import { Container } from './styles';
+import {
+    Container,
+    Image,
+    Name,
+    Description,
+    StatusContainer,
+    StatusLabel,
+} from './styles';
 
-export function OrderCard() {
+type Props = TouchableOpacityProps & {
+    index: number;
+}
+
+export function OrderCard({ index, ...rest }: Props) {
     return (
-        <Container></Container>
+        <Container index={index} {...rest}>
+            <Image source={{ uri: 'https://github.com/neanderdev.png' }} />
+
+            <Name>Pizza de Frango</Name>
+
+            <Description>
+                Mesa 5 | Qnt: 1
+            </Description>
+
+            <StatusContainer status='Pronto'>
+                <StatusLabel status='Pronto'>
+                    Pronto
+                </StatusLabel>
+            </StatusContainer>
+        </Container>
     );
 }
--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { FlatList } from 'react-native';
+
+import { OrderCard } from '@components/OrderCard';
+import { ItemSeparator } from '@components/ItemSeparator';
 
 import {
     Container,
@@ -12,6 +16,23 @@ export function Orders() {
             <Header>
                 <Title>Pedidos Feito</Title>
             </Header>
+
+            <FlatList
+                data={['1', '2', '3']}
+                keyExtractor={item => item}
+                renderItem={({ item, index }) => (
+                    <OrderCard
+                        index={index}
+                    />
+                )}
+                numColumns={2}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingHorizontal: 24,
+                    paddingBottom: 125,
+                }}
+                ItemSeparatorComponent={() => <ItemSeparator />}
+            />
         </Container>
     );
 }
