8783c5b946d2b2709375802bd4ca6fd8cefa869f
neanderdev <neander.faria@hotmail.com>
2022-08-24 15:25:28 -0300

    Listar Pedidos

--- src/components/OrderCard/index.tsx
+++ src/components/OrderCard/index.tsx
@@ -8,26 +8,37 @@ import {
     Description,
     StatusContainer,
     StatusLabel,
+    StatusTypesProps,
 } from './styles';
+
+export type OrderProps = {
+    id: string;
+    pizza: string;
+    image: string;
+    status: StatusTypesProps;
+    table_number: string;
+    quantity: string;
+};
 
 type Props = TouchableOpacityProps & {
     index: number;
+    data: OrderProps;
 }
 
-export function OrderCard({ index, ...rest }: Props) {
+export function OrderCard({ index, data, ...rest }: Props) {
     return (
         <Container index={index} {...rest}>
-            <Image source={{ uri: 'https://github.com/neanderdev.png' }} />
+            <Image source={{ uri: data.image }} />
 
-            <Name>Pizza de Frango</Name>
+            <Name>{data.pizza}</Name>
 
             <Description>
-                Mesa 5 | Qnt: 1
+                Mesa {data.table_number} | Qnt: {data.quantity}
             </Description>
 
-            <StatusContainer status='Pronto'>
-                <StatusLabel status='Pronto'>
-                    Pronto
+            <StatusContainer status={data.status}>
+                <StatusLabel status={data.status}>
+                    {data.status}
                 </StatusLabel>
             </StatusContainer>
         </Container>
--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
-import { OrderCard } from '@components/OrderCard';
+import { useAuth } from '@hooks/auth';
+
+import { OrderCard, OrderProps } from '@components/OrderCard';
 import { ItemSeparator } from '@components/ItemSeparator';
 
 import {
@@ -11,6 +14,28 @@ import {
 } from './styles';
 
 export function Orders() {
+    const [orders, setOrders] = useState<OrderProps[]>([]);
+
+    const { user } = useAuth();
+
+    useEffect(() => {
+        const subscribe = firestore()
+            .collection('orders')
+            .where('waiter_id', '==', user?.id)
+            .onSnapshot((querySnapshot) => {
+                const data = querySnapshot.docs.map((doc) => {
+                    return {
+                        id: doc.id,
+                        ...doc.data(),
+                    }
+                }) as OrderProps[];
+
+                setOrders(data);
+            });
+
+        return () => subscribe();
+    }, []);
+
     return (
         <Container>
             <Header>
@@ -18,11 +43,12 @@ export function Orders() {
             </Header>
 
             <FlatList
-                data={['1', '2', '3']}
-                keyExtractor={item => item}
+                data={orders}
+                keyExtractor={(item) => item.id}
                 renderItem={({ item, index }) => (
                     <OrderCard
                         index={index}
+                        data={item}
                     />
                 )}
                 numColumns={2}
